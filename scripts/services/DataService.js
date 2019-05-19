4ce15ff74fcbb1bbed957ba18d81ad7126d7482c
Munoon <munoongg@gmail.com>
2019-05-19 20:08:52 +0300

    Refactored DataService

--- scripts/services/DataService.js
+++ scripts/services/DataService.js
@@ -3,67 +3,31 @@ const COINS_URL = 'https://api.coinpaprika.com/v1/coins';
 const getSingleCoinUrl = id => `https://api.coinpaprika.com/v1/coins/${id}/ohlcv/today/`;
 
 const DataService = {
-  _sendRequest({ url, successCallback, method = 'GET' }) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open(method, url);
-
-    xhr.send();
-
-    xhr.onload = () => {
-      let responseData = JSON.parse(xhr.responseText);
-      successCallback(responseData);
-    }
-  },
-
-  _sendMultipleRequests(urls, callback) {
-    let requestCount = urls.length;
-    let results = [];
-
-    urls.forEach(url => {
-      DataService._sendRequest({ 
-        url, 
-        successCallback: data => {
-          results.push({ url, data });
-          requestCount--;
-
-          if (!requestCount) {
-            callback(results);
-          }
-        }
-      })
-    })
-  },
-
   getCurrencies(callback) {
-    DataService._sendRequest({
-      url: COINS_URL,
-      successCallback: data => {
-        DataService.getCurrenciesPrices(data.slice(0, 10), callback)
-      },
-    })
-  },
-
-  getCurrenciesPrices(data, callback) {
-    let coinsIds = data.map(coin => coin.id);
-    const coinsIdMap = coinsIds.reduce((acc, id) => {
-      acc[getSingleCoinUrl(id)] = id;
-      return acc;
-    }, {});
-
-    DataService._sendMultipleRequests(Object.keys(coinsIdMap), coins => {
-      const dataWithPrice = data.map(item => {
-        let itemUrl = getSingleCoinUrl(item.id);
-        let [itemPriceData] = coins.find(coin => coin.url === itemUrl).data;
-        item.price = itemPriceData.close.toFixed(2);
-        
-
-        return item;
+    fetch(COINS_URL)
+      .then(response => {
+        return response.json();
       })
+      .then(getData => {
+        const data = getData.slice(0, 10);
+        let lengh = data.length;
 
-      callback(dataWithPrice)
-    })
+        data.forEach(element => {
+          const url = getSingleCoinUrl(element.id);
+          fetch(url).then(resp => resp.json())
+            .then(getData => {
+              element.price = +getData[0].open.toFixed(2);
+              lengh--;
 
+              if (!lengh) {
+                callback(data);
+              }
+            });
+        });
+      })
+      .catch(exception => {
+        alert(exception);
+      });
   }
 }
 
